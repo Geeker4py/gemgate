8435f739cae72d56d4cf244c59bd4db3b6c96ec0
frozenfung <frozenfung@gmail.com>
2015-09-05 17:22:00 +0800

    Make it download uninstalled gems from rubygems

--- lib/gemgate/bartender.rb
+++ lib/gemgate/bartender.rb
@@ -7,11 +7,14 @@ module Gemgate
        /latest_specs.4.8.gz
        /prerelease_specs.4.8.gz
        /quick/Marshal.4.8/*.gemspec.rz
-       /gems/*.gem
     ].each do |index|
       get index do
-        redirect to transfered_index(index)
+        redirect transfered_index(index)
       end
+    end
+
+    get "/gems/*.gem" do
+      redirect source_on_rubygems
     end
 
     private
@@ -22,6 +25,14 @@ module Gemgate
       end
 
       File.join storage.get_path, index
+    end
+
+    def source_on_rubygems
+      File.join rubygems_uri, *request.path_info
+    end
+
+    def rubygems_uri
+      "https://d2chzxaqi4y7f8.cloudfront.net"
     end
 
     def storage
